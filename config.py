190d1fb769f8cfa13af250bacfa6722e4afeeb89
git4rker <unicod3r.ivan@gmail.com>
2024-07-12 13:48:21 +0300

    accidentally commited my config.py

--- config.py
+++ config.py
@@ -4,11 +4,9 @@ import time
 TOKEN = os.environ["token"]
 
 CHANNELS = {
-    "announcements": 472406978908389376
 }
 
 REQUEST_DELAY = 0
 FAILED_REQUEST_DELAY = 0
 
-# DATABASE_FILE = f"dumps/{time.strftime("%Y%m%d-%H%M%S")}.json"
-DATABASE_FILE = f"dumps/test3.json"
+DATABASE_FILE = f"dumps/{time.strftime("%Y%m%d-%H%M%S")}.json"
